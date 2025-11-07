361fbec9ae0b54253af807d4120095d38424784a
ridkhj <conserva.rdco@gmail.com>
2025-11-07 18:01:21 -0300

    ajustes para deploy: ajustanto .env do front

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_API_BASE_URL: string;
+    // adicione outras variáveis de ambiente aqui se precisar
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
